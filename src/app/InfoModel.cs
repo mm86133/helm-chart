ad4d0267d1b5c2d0d06f4583664f52ff85b488af
Mike <Mike@MainDev>
2020-03-26 09:41:29 +0000

    added dockerfile & helm

---
+++ src/app/InfoModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app
+{
+    public class InfoModel
+    {
+        public string AppEnvironment { get; set; }
+        public string AppHost { get; set; }
+    }
+}
